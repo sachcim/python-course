fd8259a08284def51e1ffbd0c52a95e51748dc83
sachcim <michalbialoz@gmail.com>
2026-03-31 21:25:13 +0200

    day-07: practical classes and oop - fixes for describe method and clean up

--- block-07-classes-and-oop/practice.py
+++ block-07-classes-and-oop/practice.py
@@ -1,10 +1,11 @@
+from typing import Optional
 class Book:
     def __init__ (self, title: str, author: str):
         self.title = title
         self.author = author
 
     def describe(self):
-        print(f'{self.title} - {self.author}')
+        return f'{self.title} - {self.author}'
 
 class Task:
     def __init__ (self, title: str, done: bool = False):
@@ -15,7 +16,7 @@ class Task:
         self.done = True
 
     def describe(self):
-        print(f'{self.title} - {"" if self.done else "nie"}wykonane')
+        return f'{self.title} - {"" if self.done else "nie"}wykonane'
 
 class BankAccount:
     def __init__ (self, owner: str, balance: float = 0):
@@ -23,39 +24,44 @@ class BankAccount:
         self.balance = balance
 
     def deposit(self, amount: float):
-        self.balance += amount
-    def withdraw(self, amount: float):
-        tryBalance = self.balance - amount
-        if tryBalance < 0:
-            print("Not enough money")
+        if amount > 0:
+            self.balance += amount
         else:
-            self.balance -= amount
+            print("Value must be positive")
+    def withdraw(self, amount: float):
+        if amount < 0:
+            print("Value must be positive")
+        else:
+            tryBalance = self.balance - amount
+            if tryBalance < 0:
+                print("Not enough money")
+            else:
+                self.balance -= amount
 
 class TaskList:
-    def __init__ (self, tasks: list[Task]):
-        self.tasks: list[Task]  = tasks
+    def __init__ (self, tasks : Optional[list[Task]] = None):
+        self.tasks = [] if tasks is None else tasks
 
     def add_task(self, task: Task):
         self.tasks.append(task)
-        print("Task added")
     def undone_tasks(self):
         undone_tasks = []
-        for index, task in enumerate(self.tasks):
+        for task in self.tasks:
             if not task.done:
                 undone_tasks.append(task)
         return undone_tasks
 
 if __name__ == "__main__":
     firstBook = Book("Pan Tadeusz", "Adam Mickiewicz")
-    firstBook.describe()
+    print(firstBook.describe())
     firstBook.title = "Balladyna"
     firstBook.author = "Juliusz Słowacki"
-    firstBook.describe()
+    print(firstBook.describe())
 
     firstTask = Task("Nauczyc się pythona")
-    firstTask.describe()
+    print(firstTask.describe())
     firstTask.mark_done()
-    firstTask.describe()
+    print(firstTask.describe())
 
     firstBankAccount = BankAccount("Michal")
     firstBankAccount.deposit(1000)
@@ -63,7 +69,7 @@ if __name__ == "__main__":
     firstBankAccount.withdraw(900)
     print(firstBankAccount.balance)
 
-    task_list = TaskList([firstTask])
+    task_list = TaskList()
     task_list.add_task(Task("Nauczyć się zip"))
     task_list.add_task(Task("Nauczyć się python lambda"))
     task_list.add_task(Task("zrobic mini project"))
@@ -74,4 +80,4 @@ if __name__ == "__main__":
 
     undone_tasks = task_list.undone_tasks()
     for task in undone_tasks:
-        task.describe()
+        print(task.describe())
